127b5accfa91cee2bd10a1b7d25dc299d6a1b5ab
Zhanar <shaimerdenovazhanar17@gmail.com>
2023-05-08 02:04:27 +0600

    edit

--- index.js
+++ index.js
@@ -2,29 +2,53 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const error = require("mongoose/lib/error");
+
+const TodoTask = require("./models/TodoTask");
 
 dotenv.config();
+
+
 
 app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.set("useFindAndModify", false);
+const db = 'mongodb+srv://TodoList:Rfrltkf7@cluster0.doy6gwm.mongodb.net/ToDoList?retryWrites=true&w=majority'
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-    console.log("Connected to db!");
-
-    app.listen(8000, ()=> console.log("Server is OK"));
-});
+mongoose
+    .connect(db)
+    .then((res) => console.log('Connected to DB'))
+    .catch((error) => console.log(error));
 
 app.set("view engine", "ejs");
 
-app.get('/',(req, res) => {
-    res.render('todolist.ejs');
+app.get("/", (req, res) => {
+    res.render("todolist.ejs");
+    
 });
 
-app.post('/',(req, res) => {
-    console.log(req.body);
+app.post('/',async (req, res) => {
+    const todoTask = new TodoTask({
+        content: req.body.content
+    });
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.redirect("/");
+    }
 });
 
-// app.listen(8000, ()=> console.log("Server is OK"));
+
+
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+  });
+        
+
+app.listen(8000, ()=> console.log("Server is OK"));
